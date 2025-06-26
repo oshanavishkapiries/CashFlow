76cc06fca2b047cb87e865c23c535da85bd0686b
153918928+oshanavishkapiries@users.noreply.github.com <153918928+oshanavishkapiries@users.noreply.github.com>
2025-06-26 13:32:50 +0530

    change-to-next-js

--- src/layout/Main.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainLayout = ({ children }: React.PropsWithChildren) => {
-  return <main className="min-h-screen max-w-md mx-auto bg-dark_1 relative px-3">{children}</main>;
-};
-
-export default MainLayout;
